249493621846cc7659bbf66cd6929ead6f84d86a
Alexandre <alexandrepeluchi@gmail.com>
2019-12-22 11:57:40 -0200

    Created a controller for the to-do list

---
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index() 
+        {
+
+        }
+        
+    }
+}
