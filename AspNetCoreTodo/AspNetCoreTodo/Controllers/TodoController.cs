45fb37eb3951e48ef15fe02e88b4e1afde3609e9
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2019-12-23 10:07:16 -0300

    Making the controller interact with the interface

--- AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/AspNetCoreTodo/Controllers/TodoController.cs
@@ -1,12 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
+using AspNetCoreTodo.Services;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class TodoController : Controller
     {
-        public IActionResult Index() 
-        {
+        private readonly ITodoItemService _todoItemService;
 
+        public TodoController (ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            var items = await _todoItemService.GetIncompleteItemAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = items
+            };
+
+            return View(model);
         }
         
     }
